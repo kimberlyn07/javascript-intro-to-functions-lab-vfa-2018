229ede327fa957a7d912fa9573826c9bc4d6cf5f
Kimberly Nguyen <kimberlyainguyen@gmail.com>
2018-07-01 01:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function logShout(string){
 function logWhisper(string){
   console.log( string.toLowerCase())
 }
+function sayHiToGrandma(string){
+    var uppercase = "HELLO"
+    var lowercase = "hello"
+    
+    if (string === string.toLowerCase()){
+      return "I can't hear you!"}
+    else if (string === string.toUpperCase()){
+      return "YES INDEED!"}
+    else if (string === "I love you, Grandma."){
+      return "I love you,too."}
+    }
